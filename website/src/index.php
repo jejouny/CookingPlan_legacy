feb9877a7d5e41e1781bcefbbd3cdbac27bff130
jejouny <j.joubert@netcourrier.com>
2016-05-26 16:37:22 +0200

    Draw the main page banner

--- website/src/index.php
+++ website/src/index.php
@@ -14,6 +14,7 @@ header("location: profile.php");
    </head>
    <body>
       <div id="main">
+         <img src="banner.svg" style="max-width: 100%"/>
          <h1>Planning des repas</h1>
          <div id="login">
             <form action="" method="post">
